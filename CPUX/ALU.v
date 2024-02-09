170e57b93567da41d2b24e01692698e24a9c27c2
zbonneau <zbonneau@ttu.edu>
2024-02-08 22:55:16 -0600

    typo, switched "W" to "B" for Byte format instructions

--- CPUX/ALU.v
+++ CPUX/ALU.v
@@ -15,7 +15,7 @@ module ALUX(
 
         case({instr[15:12], 5'b0000_0, instr[6], 6'b00_0000})
             ADDRESS: begin
-
+                
             end
 
             FORMATII: begin
@@ -31,7 +31,7 @@ module ALUX(
                 {Cout, Vout, Nout, Zout} <= {Cin, Vin, Nin, Zin};
             end
 
-            MOVW: begin
+            MOVB: begin
                 result <= {12'h0, src[7:0]};
                 {Cout, Vout, Nout, Zout} <= {Cin, Vin, Nin, Zin};
             end
@@ -43,7 +43,7 @@ module ALUX(
                 Vout = (src[15] & dst[15] & ~result[15] | ~src[15] & ~dst[15] & result[15]);
             end
 
-            ADDW: begin
+            ADDB: begin
                 {Cout, result[7:0]} = src[7:0]+dst[7:0];
                 Nout = result[7];
                 Zout = (result == 0);
@@ -57,7 +57,7 @@ module ALUX(
                 Vout = (src[15] & dst[15] & ~result[15] | ~src[15] & ~dst[15] & result[15]);
             end
 
-            ADDCW: begin
+            ADDCB: begin
                 {Cout, result[7:0]} = src[7:0]+dst[7:0] + Cin;
                 Nout = result[7];
                 Zout = (result == 0);
@@ -72,7 +72,7 @@ module ALUX(
                       // neg    -  pos     =  pos       OR  pos     - neg     = neg
             end
 
-            SUBCW: begin
+            SUBCB: begin
                 {Cout, result[7:0]} = dst[7:0] - src[7:0] + 1 + Cin;
                 Nout = result[7];
                 Zout = (result == 0);
@@ -87,7 +87,7 @@ module ALUX(
                 Vout = (dst[15] & ~src[15] & ~result[15] | ~dst[15] & src[15] & result[15]);
             end
 
-            SUBW, CMPW: begin
+            SUBB, CMPB: begin
                 {Cout, result[7:0]} = dst[7:0] - src[7:0];
                 Nout = result[7];
                 Zout = (result == 0);
@@ -100,19 +100,19 @@ module ALUX(
                 // See DADD_test.v for explanation of this block.
                 // TL;DR causes carry through nibbles on decimal overflow rather than hex overflow
                 {Cout, result[3:0]}   = (dst[3:0]   + src[3:0]   + Cout < 10) ? dst[3:0]   + src[3:0]   + Cout
-                                                                            : dst[3:0]   + src[3:0]   + Cout + 6;
+                                                                              : dst[3:0]   + src[3:0]   + Cout + 6;
                 {Cout, result[7:4]}   = (dst[7:4]   + src[7:4]   + Cout < 10) ? dst[7:4]   + src[7:4]   + Cout
-                                                                            : dst[7:4]   + src[7:4]   + Cout + 6;
+                                                                              : dst[7:4]   + src[7:4]   + Cout + 6;
                 {Cout, result[11:8]}  = (dst[11:8]  + src[11:8]  + Cout < 10) ? dst[11:8]  + src[11:8]  + Cout
-                                                                            : dst[11:8]  + src[11:8]  + Cout + 6;
+                                                                              : dst[11:8]  + src[11:8]  + Cout + 6;
                 {Cout, result[15:12]} = (dst[15:12] + src[15:12] + Cout < 10) ? dst[15:12] + src[15:12] + Cout
-                                                                            : dst[15:12] + src[15:12] + Cout + 6;
+                                                                              : dst[15:12] + src[15:12] + Cout + 6;
                 Nout = result[15];
                 Zout = (result == 0);
                 Vout = 0; // Undefined by MSP430 Family Guide
             end
 
-            DADDW: begin
+            DADDB: begin
                 Cout = Cin;
 
                 {Cout, result[3:0]}   = (dst[3:0]   + src[3:0]   + Cout < 10) ? dst[3:0]   + src[3:0]   + Cout
@@ -132,7 +132,7 @@ module ALUX(
                 Vout = 0; // defined as 0 by MSP430 Family Guide
             end
 
-            BITW, ANDW: begin
+            BITB, ANDB: begin
                 result[7:0] = src[7:0] & dst[7:0];
                 Nout = result[7];
                 Zout = (result == 0);
@@ -148,7 +148,7 @@ module ALUX(
                 Vout = Vin;
             end
 
-            BICW: begin
+            BICB: begin
                 result[7:0] = ~src[7:0] & dst[7:0];
                 Nout = Nin;
                 Zout = Zin;
@@ -164,7 +164,7 @@ module ALUX(
                 Vout = Vin;
             end
 
-            BISW: begin
+            BISB: begin
                 result[7:0] = src[7:0] | dst[7:0];
                 Nout = Nin;
                 Zout = Zin;
@@ -180,7 +180,7 @@ module ALUX(
                 Vout = src[15] & dst[15]; 
             end
 
-            XORW: begin
+            XORB: begin
                 result[7:0] = src[7:0] ^ dst[7:0];
                 Nout = result[7];
                 Zout = (result == 0);
