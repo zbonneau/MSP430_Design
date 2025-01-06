f79895bd0a308b433cebf6457a6194fd36d9794a
zbonneau <zbonneau@ttu.edu>
2025-01-05 23:11:55 -0600

    Operand Fetch and Constant generator designs

---
+++ NEW/ConstantGenerator.v
@@ -0,0 +1,78 @@
+/*--------------------------------------------------------
+    Module Name : Constant Generator
+    Description: 
+        Describes the MSP430 CPU Constant Generator.
+        Certain Immediate constants are more common than others.
+        Generating these values in silicon is faster and more 
+        memory efficient than storing them in program memory.
+        The constant generator utilizes two registers (R2/R3 - 
+        CG1/CG2) and various address modes to produce source
+        operands.
+    
+    Inputs:
+        IW - Instruction Word used for decoding 1- vs 2-operand 
+             instructions
+        srcA, As - Combinations of these IW bits produce CGx output
+        dstA, Ad - Same as above. Less common, but possible for 
+                   instructions such as Push, Call, etc 
+
+    Outputs: 
+        src, dst - src & dst operand data
+        srcGenerated, dstGenerated - 
+            notify external modules that the CG triggered
+
+--------------------------------------------------------*/
+
+module ConstantGenerator(
+    input [15:0] IW,
+    input [3:0]  srcA,
+    input [1:0]  As,
+    input [3:0]  dstA,
+    input        Ad,
+    output reg [15:0] src, dst,
+    output reg srcGenerated, dstGenerated
+ );
+ `include "NEW\\MACROS.v"
+
+ // Initialize regs
+    initial begin {src, dst, srcGenerated, dstGenerated} = 0; end
+
+ always @(*) begin
+    // default behavior
+    {src, dst, srcGenerated, dstGenerated} = 0;
+
+    if (|IW[15:14]) begin
+        // 2-op instruction decoding
+        case (srcA, As)
+            {CG1, INDEXED_MODE}:                begin src <= 16'h0;     srcGenerated <= 1; end
+            {CG1, INDIRECT_MODE}:               begin src <= 16'h4;     srcGenerated <= 1; end
+            {CG1, INDIRECT_AUTOINCREMENT_MODE}: begin src <= 16'h8;     srcGenerated <= 1; end
+            {CG2, REGISTER_MODE}:               begin src <= 16'h0;     srcGenerated <= 1; end
+            {CG2, INDEXED_MODE}:                begin src <= 16'h1;     srcGenerated <= 1; end
+            {CG2, INDIRECT_MODE}:               begin src <= 16'h2;     srcGenerated <= 1; end
+            {CG2, INDIRECT_AUTOINCREMENT_MODE}: begin src <= 16'hFFFF;  srcGenerated <= 1; end
+            default:                            begin src <= 16'h0;     srcGenerated <= 0; end
+        endcase
+        case (dstA, Ad)
+            {CG1, 1'b1}: begin dst <= 16'h0; dstGenerated <= 1; end
+            {CG2, 1'b0}: begin dst <= 16'h0; dstGenerated <= 1; end
+            {CG2, 1'b1}: begin dst <= 16'h1; dstGenerated <= 1; end
+            default:     begin dst <= 16'h0; dstGenerated <= 0; end
+        endcase
+    end
+    else begin
+        // 1-op & jump instructions
+        case (dstA, As) // As bits of IW are used by Rdst for generation. See 
+                        // CPU (16-bit) - Instruction Set - Single Operand
+            {CG1, INDEXED_MODE}:                begin src <= 16'h0;     srcGenerated <= 1; end
+            {CG1, INDIRECT_MODE}:               begin src <= 16'h4;     srcGenerated <= 1; end
+            {CG1, INDIRECT_AUTOINCREMENT_MODE}: begin src <= 16'h8;     srcGenerated <= 1; end
+            {CG2, REGISTER_MODE}:               begin src <= 16'h0;     srcGenerated <= 1; end
+            {CG2, INDEXED_MODE}:                begin src <= 16'h1;     srcGenerated <= 1; end
+            {CG2, INDIRECT_MODE}:               begin src <= 16'h2;     srcGenerated <= 1; end
+            {CG2, INDIRECT_AUTOINCREMENT_MODE}: begin src <= 16'hFFFF;  srcGenerated <= 1; end
+            default:                            begin src <= 16'h0;     srcGenerated <= 0; end
+        endcase
+    end
+ end
+endmodule
--- NEW/OperandFetch.v
+++ NEW/OperandFetch.v
@@ -15,3 +15,48 @@
         address  - memory address for operand fetch, result write
         
 --------------------------------------------------------*/
+
+module OperandFetch(
+    input clk, rst,
+    input [15:0] Rsrc, Rdst, MDB, 
+    input srcM, srcL, dstM, dstL,
+    input [1:0] AddrM,
+    input AddrL, IdxM,
+    output [15:0] OpSrc, OpDst, MAB
+ );
+
+ // Internal Regs for various address modes
+    reg [15:0] src, dst, Addr;
+
+ // Initialize
+    initial begin {src, dst, Addr} = 0; end
+
+ // Combinational Logic - outputs to MAB, Func Unit
+    assign OpSrc = srcM ? src : Rsrc;
+    assign OpDst = dstM ? dst : Rdst;
+    assign MAB   = (AddrM == 0) ? Addr :
+                   (AddrM == 1) ? Rsrc - 2 :
+                   (AddrM == 2) ? Rsrc : 
+                   (AddrM == 3) ? Rdst;
+
+ // Sequential Logic - src, dst, Addr for various address modes
+    always @(posedge clk) begin
+        if (rst) begin
+            {src, dst, Addr} = 0;
+        end
+        else begin
+            if (srcL) begin
+                src <= MDB;
+            end
+            if (dstL) begin
+                dst <= dstM ? MDB : Rdst;
+            end
+            if (AddrL) begin
+                Addr = (AddrM == 0 && IdxM == 0) ? MDB + Rsrc :
+                       (AddrM == 0 && IdxM == 1) ? MDB + Rdst :
+                       (AddrM == 3) ? Rdst :
+                       Addr; // Default to no Change
+            end
+        end
+    end
+endmodule
