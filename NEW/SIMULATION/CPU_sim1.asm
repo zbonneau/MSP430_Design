c0c04e23d17945e4c06ed41379bbdbba66824bd0
zbonneau <zbonneau@ttu.edu>
2025-02-07 00:42:55 -0600

    First CPU simulation complete. approx 75 CC of execution (75 uS), covering 2-op, 1-op, push, and various address modes.

---
+++ NEW/SIMULATION/CPU_sim1.asm
@@ -0,0 +1,69 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+			mov.w 	#0x1234, R4
+			add.w	#0x0008, R4
+			mov.b	R4,		 R5
+
+
+			mov.w 	var1,  	R6
+			mov.w	#var1, 	R7
+
+			sub.w	@R7+,  	R8
+			add		@R7, 	var1+2
+			push.w	R4
+			push.b	R4
+
+			mov.w 	#5, R9
+L1:			dec 	R9
+			jnz		L1
+
+			mov.w	#0x20, R10
+L2:			RRC		R10
+			jnc		L2
+
+end:		jmp 	end
+			nop
+
+
+
+
+var1 .word	0xa5a5, 0x2244
+
+                                            
+
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
--- NEW/SIMULATION/CPU_tb.v
+++ NEW/SIMULATION/CPU_tb.v
@@ -19,6 +19,14 @@ wire [15:0] MAB;
 wire [15:0] MDBout;
 wire MW, BW, INTACK;
 
+/* Useful Construction in iVerilog simulator. Arrays are not
+ * exported by the vvp simulator
+ */
+wire [15:0] Reg0, Reg1, Reg2, Reg3, Reg4, Reg5, Reg6, Reg7,
+      Reg8, Reg9, Reg10, Reg11, Reg12, Reg13, Reg14, Reg15;
+
+`include "NEW\\MACROS.v"
+
 initial begin 
     {MCLK, reset} = 0; 
     NMI = 0;
@@ -26,23 +34,58 @@ initial begin
     IntAddrLSBs = 63; // maps to FFFE, reset vector
 end
 
+reg [15:0] exit;
+
 
 /* Simple Memory File Construct */
 reg [7:0] memory[65535:17408];
+integer i;
 
 initial begin
-    for (integer i = 16'h4400; i < 17'h1_0000; i = i + 2) 
-        {memory[i+1], memory[i]} <= 16'h4303; // default NOP
+    for (i = 16'h4400; i < 17'h1_0000; i = i + 2) 
+        {memory[i+1], memory[i]} = 16'h4303; // default NOP
+    i = 16'h4400;
     
     /* load program memory here */
-
+    {memory[i+1], memory[i]} = 16'h4031; i = i+2;
+    {memory[i+1], memory[i]} = 16'h2400; i = i+2;
+    {memory[i+1], memory[i]} = 16'h40B2; i = i+2;
+    {memory[i+1], memory[i]} = 16'h5A80; i = i+2;
+    {memory[i+1], memory[i]} = 16'h015C; i = i+2;
+    {memory[i+1], memory[i]} = 16'h4034; i = i+2;
+    {memory[i+1], memory[i]} = 16'h1234; i = i+2;
+    {memory[i+1], memory[i]} = 16'h5234; i = i+2;
+    {memory[i+1], memory[i]} = 16'h4445; i = i+2;
+    {memory[i+1], memory[i]} = 16'h4016; i = i+2;
+    {memory[i+1], memory[i]} = 16'h0024; i = i+2;
+    {memory[i+1], memory[i]} = 16'h4037; i = i+2;
+    {memory[i+1], memory[i]} = 16'h4438; i = i+2;
+    {memory[i+1], memory[i]} = 16'h8738; i = i+2;
+    {memory[i+1], memory[i]} = 16'h57A0; i = i+2;
+    {memory[i+1], memory[i]} = 16'h001C; i = i+2;
+    {memory[i+1], memory[i]} = 16'h1204; i = i+2;
+    {memory[i+1], memory[i]} = 16'h1244; i = i+2;
+    {memory[i+1], memory[i]} = 16'h4039; i = i+2;
+    {memory[i+1], memory[i]} = 16'h0005; i = i+2;
+    {memory[i+1], memory[i]} = 16'h8319; i = i+2;
+    {memory[i+1], memory[i]} = 16'h23FE; i = i+2;
+    {memory[i+1], memory[i]} = 16'h403A; i = i+2;
+    {memory[i+1], memory[i]} = 16'h0020; i = i+2;
+    {memory[i+1], memory[i]} = 16'h100A; i = i+2;
+    {memory[i+1], memory[i]} = 16'h2BFE; i = i+2;
+    {memory[i+1], memory[i]} = 16'h3FFF; i = i+2; exit = i;
+    {memory[i+1], memory[i]} = 16'h4303; i = i+2;
+    {memory[i+1], memory[i]} = 16'hA5A5; i = i+2;
+    {memory[i+1], memory[i]} = 16'h2244; i = i+2;
 
 
     {memory[16'hFFFF], memory[16'hFFFE]} = 16'h4400;
 end
 
-assign MDBin = BW ? {8'h00,         memory[MAB]} :
-                    {memory[MAB+1], memory[MAB]};
+assign MDBin = (MAB >= 16'h4400) ?
+                    BW ? {8'h00,         memory[MAB]}
+                       : {memory[MAB+1], memory[MAB]}
+                                 : 16'h0000;
 
 /* End Memory */
 
@@ -54,6 +97,23 @@ CPU uut
     .MW(MW), .BW(BW), .INTACK(INTACK)
 );
 
+assign Reg0  = uut.RFile.Registers[R0];
+assign Reg1  = uut.RFile.Registers[R1];
+assign Reg2  = uut.RFile.Registers[R2];
+assign Reg3  = uut.RFile.Registers[R3];
+assign Reg4  = uut.RFile.Registers[R4];
+assign Reg5  = uut.RFile.Registers[R5];
+assign Reg6  = uut.RFile.Registers[R6];
+assign Reg7  = uut.RFile.Registers[R7];
+assign Reg8  = uut.RFile.Registers[R8];
+assign Reg9  = uut.RFile.Registers[R9];
+assign Reg10 = uut.RFile.Registers[R10];
+assign Reg11 = uut.RFile.Registers[R11];
+assign Reg12 = uut.RFile.Registers[R12];
+assign Reg13 = uut.RFile.Registers[R13];
+assign Reg14 = uut.RFile.Registers[R14];
+assign Reg15 = uut.RFile.Registers[R15];
+
 localparam CLK_PERIOD = 10;
 always #(CLK_PERIOD/2) MCLK = ~MCLK;
 
@@ -63,12 +123,14 @@ initial begin
 end
 
 initial begin
-    IntAddr = 63;
+    IntAddrLSBs = 63;
     reset = 1; #(3*CLK_PERIOD); reset = 0; 
     while (~INTACK) #CLK_PERIOD;
 
     /* Program has entered reset vector at this point */
-    #(100*CLK_PERIOD);
+    while (uut.RFile.Registers[PC] != exit)
+        #CLK_PERIOD;
+    #(3*CLK_PERIOD);
     $finish(0);
 end
 endmodule
