cbbd3cc5117726512eb95ab50ccaff628cbf6a80
zbonneau <zbonneau@ttu.edu>
2025-02-11 23:29:35 -0600

    macro field name altered, () added

--- NEW/MACROS.v
+++ NEW/MACROS.v
@@ -1,9 +1,9 @@
 `ifndef MACROS
 `define MACROS
 
-`define PULSE_VAL_DELAY(x,val,delay) begin x = val; #delay; x = 0; end
-`define PULSE_VAL(x,val) `PULSE_VAL_DELAY(x,val,10)
-`define PULSE_DELAY(x,delay) `PULSE_VAL_DELAY(x,1,delay)
-`define PULSE(x) `PULSE_VAL_DELAY(x,1,10)
+`define PULSE_VAL_DELAY(thing,val,delay) begin (thing) = (val); #(delay); (thing) = 0; end
+`define PULSE_VAL(thing,val) `PULSE_VAL_DELAY(thing,val,10)
+`define PULSE_DELAY(thing,delay) `PULSE_VAL_DELAY(thing,1,delay)
+`define PULSE(thing) `PULSE_VAL_DELAY(thing,1,10)
 
 `endif // MACROS
